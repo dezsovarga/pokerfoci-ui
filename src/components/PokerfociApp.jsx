030561d29371fed607f950a41e05c94e09bc06ff
Varga Dezso <dezso.varga.dev@gmail.com>
2022-07-31 13:32:34 +0300

    refactored to functional components

--- src/components/PokerfociApp.jsx
+++ src/components/PokerfociApp.jsx
@@ -1,33 +1,31 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import LoginContainer from './authentication/login/LoginContainer.jsx'
 import SignupContainer from './authentication/signup/SignupContainer.jsx'
 
+const PokerfociApp = (props) => {
 
-class PokerfociApp extends Component {
-    render() {
-        return (
-            <div className="TodoApp">
-                <Router>
-                    <>
-                        {/* <HeaderComponent/> */}
-                        <Routes>
-                            <Route path="/" exact element={<LoginContainer/>}/>
-                            <Route path="login" element={<LoginContainer/>}/>
-                            <Route path="/signup" element={<SignupContainer/>}/>
-                            {/*<AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
-                            <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
-                            <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
+    return (
+        <div className="TodoApp">
+            <Router>
+                <>
+                    {/* <HeaderComponent/> */}
+                    <Routes>
+                        <Route path="/" exact element={<LoginContainer/>}/>
+                        <Route path="login" element={<LoginContainer/>}/>
+                        <Route path="/signup" element={<SignupContainer/>}/>
+                        {/*<AuthenticatedRoute path="/todos/:id" component={TodoComponent}/>
+                        <AuthenticatedRoute path="/todos" component={ListTodosComponent}/>
+                        <AuthenticatedRoute path="/logout" component={LogoutComponent}/>
 
-                            <Route component={ErrorComponent}/> */}
-                        </Routes>
-                        {/* <FooterComponent/> */}
-                    </>
-                </Router>
-                {/* <LoginContainer/> */}
-            </div>
-        )
-    }
+                        <Route component={ErrorComponent}/> */}
+                    </Routes>
+                    {/* <FooterComponent/> */}
+                </>
+            </Router>
+            {/* <LoginContainer/> */}
+        </div>
+    )
 }
 
 export default PokerfociApp
--- src/components/authentication/login/LoginContainer.jsx
+++ src/components/authentication/login/LoginContainer.jsx
@@ -1,82 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './LoginContainer.css';
 
-class LoginContainer extends Component {
+const LoginContainer = (props) => {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            username: 'in28minutes',
-            password: '',
-            hasLoginFailed: false,
-            showSuccessMessage: false
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.loginClicked = this.loginClicked.bind(this)
-    }
-
-    handleChange(event) {
-        //console.log(this.state);
-        this.setState(
-            {
-                [event.target.name]
-                    : event.target.value
-            }
-        )
-    }
-
-    loginClicked() {
-//             AuthenticationService
-//                 .executeJwtAuthenticationService(this.state.username, this.state.password)
-//                 .then((response) => {
-//                     AuthenticationService.registerSuccessfulLoginForJwt(this.state.username, response.data.token)
-//                     this.props.history.push(`/welcome/${this.state.username}`)
-//                 }).catch(() => {
-//                     this.setState({ showSuccessMessage: false })
-//                     this.setState({ hasLoginFailed: true })
-//                 })
+    const submitHandler = (event) => {
         console.log("loginclicked")
 
     }
 
-    
-    render() {
-        return (
-            // <div>
-            //     <h1>Login</h1>
-            //     <div className="container">
-            //         {this.state.hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
-            //         {this.state.showSuccessMessage && <div>Login Sucessful</div>}
-            //         User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} required/>
-            //         Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required/>
-            //         <button className="btn btn-success" onClick={this.loginClicked}>Login</button>
-            //     </div>
-            // </div>
-            <div className="login-form">
-                    <h3 className="text-center">Sign in to Pokerfoci</h3>       
-                <form action="/examples/actions/confirmation.php" method="post">
-                    <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Username" required="required" />
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Password" required="required" />
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-primary btn-block">Log in</button>
-                    </div>
-                    <div className="clearfix">
-                        <label className="float-left form-check-label"><input type="checkbox" /> Remember me</label>
-                        <a href="#" className="float-right">Forgot Password?</a>
-                    </div>        
-                </form>
-                <p className="text-center">
-                    <Link className="nav-link" to="/signup">Create an Account</Link>
-                </p>
-            </div>
-        )
-    }
+    return (
+        <div className="login-form">
+                <h3 className="text-center">Sign in to Pokerfoci</h3>       
+            <form onSubmit={submitHandler}>
+                <div className="form-group">
+                    <input type="text" className="form-control" placeholder="Username" required="required" />
+                </div>
+                <div className="form-group">
+                    <input type="password" className="form-control" placeholder="Password" required="required" />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-primary btn-block">Log in</button>
+                </div>
+                <div className="clearfix">
+                    <label className="float-left form-check-label"><input type="checkbox" /> Remember me</label>
+                    <a href="#" className="float-right">Forgot Password?</a>
+                </div>        
+            </form>
+            <p className="text-center">
+                <Link className="nav-link" to="/signup">Create an Account</Link>
+            </p>
+        </div>
+    )
 }
 
 export default LoginContainer
