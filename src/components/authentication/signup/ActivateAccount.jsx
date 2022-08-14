594adba2dff8a05bf93d76021a9bf789f7199ea6
Varga Dezso <dezso.varga.dev@gmail.com>
2022-08-14 22:53:05 +0300

    already confirmed flow

--- src/components/authentication/signup/ActivateAccount.jsx
+++ src/components/authentication/signup/ActivateAccount.jsx
@@ -8,31 +8,53 @@ import { Link } from 'react-router-dom';
 const ActivateAccount = (props) => {
 
     const { confirmToken } = useParams();
-    const [loading, setLoading] = useState(true); 
+    const [loading, setLoading] = useState(false); 
+    const [alreadyConfirmed, setAlreadyConfirmed] = useState(false); 
+
 
     async function activateAccount(confirmToken) {
-        const response = await fetch(`http://localhost:8081/account/register/confirm/${confirmToken}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        const data = await response.json();
-        console.log(data);
-        setLoading(false);
-        //TODO: forward to activation confirmed or homepage page
+      setLoading(true);
+      const response = await fetch(`http://localhost:8081/account/register/confirm/${confirmToken}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      const data = await response.json();
+      console.log(data);
+      setLoading(false);
+      if (response.status === 409) {
+        setAlreadyConfirmed(true);
+      }
     }
 
     useEffect(() => {
         activateAccount(confirmToken);
-    }, []);  
+    }, [confirmToken]);  
 
     const SuccesfullyConfirmed = () => {
       return (
         <section>
           <h2 className={classes.title}>Congratulations! Your account has been confirmed succesfully!</h2>
-          <img className={classes.confirmIcon} src={confirmIcon} />
-          <div className={classes.subtitle}>You can now login in the appiication</div>
+          <img alt="confirmIcon" className={classes.confirmIcon} src={confirmIcon} />
+          <div className={classes.subtitle}>You can now login in the application</div>
+        </section>
+      );
+    }
+
+    const AlreadyConfirmed = () => {
+      return (
+          <h2 className={classes.title}>Your account has already been confirmed!</h2>
+      );
+    }
+
+    const ConfirmationFeedback = () => {
+      return (
+        <section>
+          {alreadyConfirmed && <AlreadyConfirmed></AlreadyConfirmed>}
+          {!alreadyConfirmed && <SuccesfullyConfirmed></SuccesfullyConfirmed>}
+
+          {/* //TODO: create homepage page */}
           <Link className="nav-link" to={`/home`}>
                 <button type="submit" className="btn btn-primary"> 
                     Continue to homepage
@@ -44,7 +66,8 @@ const ActivateAccount = (props) => {
 
     return (
       <React.Fragment>
-        {loading && <SuccesfullyConfirmed></SuccesfullyConfirmed>}
+        {loading && <p>Loading...</p>}
+        {!loading && <ConfirmationFeedback></ConfirmationFeedback>}
         
       </React.Fragment>
     );
