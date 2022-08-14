d296ae886b9f1c8c82b93dbb37ad5f671997f937
Varga Dezso <dezso.varga.dev@gmail.com>
2022-08-14 22:26:23 +0300

    added succesful confirmation message

--- src/components/authentication/signup/ActivateAccount.jsx
+++ src/components/authentication/signup/ActivateAccount.jsx
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import React from 'react';
 import { useParams } from 'react-router-dom';
-
+import classes from './ConfirmSignup.module.css';
+import confirmIcon from '../../../assets/check.png';
+import { Link } from 'react-router-dom';
 
 const ActivateAccount = (props) => {
 
     const { confirmToken } = useParams();
+    const [loading, setLoading] = useState(true); 
 
     async function activateAccount(confirmToken) {
         const response = await fetch(`http://localhost:8081/account/register/confirm/${confirmToken}`, {
@@ -16,14 +19,35 @@ const ActivateAccount = (props) => {
         });
         const data = await response.json();
         console.log(data);
+        setLoading(false);
         //TODO: forward to activation confirmed or homepage page
-      }
+    }
 
-      useEffect(() => {
+    useEffect(() => {
         activateAccount(confirmToken);
-      }, []);  
+    }, []);  
 
-      return null;
+    const SuccesfullyConfirmed = () => {
+      return (
+        <section>
+          <h2 className={classes.title}>Congratulations! Your account has been confirmed succesfully!</h2>
+          <img className={classes.confirmIcon} src={confirmIcon} />
+          <div className={classes.subtitle}>You can now login in the appiication</div>
+          <Link className="nav-link" to={`/home`}>
+                <button type="submit" className="btn btn-primary"> 
+                    Continue to homepage
+                </button>
+            </Link>
+        </section>
+      );
+    }
+
+    return (
+      <React.Fragment>
+        {loading && <SuccesfullyConfirmed></SuccesfullyConfirmed>}
+        
+      </React.Fragment>
+    );
 }
 
 export default ActivateAccount;
