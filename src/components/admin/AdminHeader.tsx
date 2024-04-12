297179da15bf0daf18f9071de49f0fd3c377b704
Varga Dezso <dezso.varga.ext@lastminute.com>
2024-04-12 15:15:56 +0300

    refactoring admin header buttons

--- src/components/admin/AdminHeader.tsx
+++ src/components/admin/AdminHeader.tsx
@@ -1,21 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {Col, Row} from "react-bootstrap";
+import classes from "./AdminHeader.module.css";
 
 const AdminHeader = () => {
 
     return (
-        <section>
-            <nav className="navbar navbar-expand-md justify-content-center">
-                <Link className="nav-link" to="/admin-page/events" data-testid='home-link'>
-                    <button className="btn btn-light">Events </button>
-                </Link>
-                <Link className="nav-link" to="/admin-page/players" data-testid='home-link'>
-                    <button className="btn btn-light">Players </button>
-                </Link>
-            </nav>
+        <section >
+            <container >
+                <Row className={classes.admin_header_menu}>
+                    <Col className={classes.admin_header_menu_buttons} xs={6} md={6} lg={6}>
+                        <Link className="nav-link" to="/admin-page/events" data-testid='home-link'>
+                            Events
+                        </Link>
+                    </Col>
+                    <Col className={classes.admin_header_menu_buttons} xs={6} md={6} lg={6} >
+                        <Link className="nav-link" to="/admin-page/players" data-testid='home-link'>
+                            Players
+                        </Link>
+                    </Col>
+                </Row>
+            </container>
         </section>
     )
-
 }
 
 export default AdminHeader;
